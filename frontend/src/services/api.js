5b639adbf14b9a7014b1d6c15ab2b1a8ac2c139f
VISHNUMANIKANDAN T <155707903+VishnuMK2006@users.noreply.github.com>
2025-12-06 21:30:44 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //base
-const API_BASE_URL = "https://inventory-management-yexl.onrender.com/api";
+const API_BASE_URL = "https://inventory-management-yeso.onrender.com/api";
 //  const API_BASE_URL = "http://localhost:5000/api";
 
 const api = axios.create({
