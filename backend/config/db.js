47ff6ed8709696c8defb0308f9127c6b601a33ef
Vishnumanikandan <vishnumanikandan654@gmail.com>
2025-12-07 16:13:12 +0530

    DB url update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
-// const monbodburl="mongodb://localhost:27017/";
-const monbodburl="mongodb+srv://vishnumanikandan:y9CF5NWxWDWErRTJ@cluster0.dt9pqrb.mongodb.net/?appName=Cluster0";
+
+const monbodburl = "mongodb+srv://vishnumanikandan:y9CF5NWxWDWErRTJ@cluster0.dt9pqrb.mongodb.net/test?retryWrites=true&w=majority";
+
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(monbodburl);
-    console.log(`MongoDB Connected: ${monbodburl}`);
+    console.log(`MongoDB Connected Successfully`);
   } catch (error) {
-    console.error(error);
+    console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
